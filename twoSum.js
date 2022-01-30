66923551740baf669288635825dfb3b71a81a3c9
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-01-30 12:20:34 -0800

    completed twoSum

---
+++ twoSum.js
@@ -0,0 +1,17 @@
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//example: Given nums = [2, 7, 11, 15], target = 9,
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+
+const findNums = (nums, target) => {
+  for(let i = 0; i < nums.length; i++){
+    const total = nums[i] + nums[i+1];
+    if(total === target){
+      console.log(i, i+1)
+    }
+  }
+}
+findNums([2, 7, 11, 15], 9);
