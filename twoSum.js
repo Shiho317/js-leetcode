1e9a0c3d1bb905bb5783f5ce01e39928fc535cf0
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-02-03 21:17:12 -0800

    added addTwoNumbers js

--- twoSum.js
+++ twoSum.js
@@ -6,12 +6,36 @@
 //Because nums[0] + nums[1] = 2 + 7 = 9,
 //return [0, 1].
 
-const findNums = (nums, target) => {
-  for(let i = 0; i < nums.length; i++){
-    const total = nums[i] + nums[i+1];
-    if(total === target){
-      console.log(i, i+1)
+const findNums = (arr, total) => {
+
+  let resultArr;
+
+  for(let i = 0; i < arr.length; i++){
+    const targetNum = total - arr[i];
+    const numIndex = arr.indexOf(targetNum);
+
+    if(numIndex >= 0){
+      resultArr = [arr[i], arr[numIndex]]
     }
   }
+
+  return resultArr
 }
-findNums([2, 7, 11, 15], 9);
+
+const givenNums = [2, 7, 11, 15];
+
+console.log(findNums(givenNums, 9))
+
+
+// const findNums = (nums, target) => {
+//   for(let i = 0; i < nums.length; i++){
+//     const total = nums[i] + nums[i+1];
+//     if(total === target){
+//       console.log(i, i+1)
+//     }
+//   }
+// }
+// findNums([2, 7, 11, 15], 9);
+
+
+
