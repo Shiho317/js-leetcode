39aa212c6d90105cc568e784c5b32b717360b997
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-02-04 09:50:27 -0800

    added answer

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -3,3 +3,34 @@
 //You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
 // linked list
+
+const addTwoNums = (num1, num2) => {
+  let arr = [];
+  let total;
+
+  const reNum1 = num1.reverse();
+  const reNum2 = num2.reverse();
+
+  for(let i = 0; i < reNum1.length; i++){
+    const calcNum = reNum1[i] + reNum2[i];
+
+    if(calcNum >= 10){
+        arr[i - 1] += 1
+        total = calcNum  - 10;
+      
+    }else{
+      total = calcNum
+    }
+
+    arr.push(total)
+  }
+
+  const reArr = arr.reverse()
+
+  return reArr
+};
+
+const num1 = [2, 4, 3];
+const num2 = [5, 6, 4];
+
+console.log(addTwoNums(num1, num2))
