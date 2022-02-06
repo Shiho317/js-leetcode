dfe9266a39fed68a130b182c28ea6bca118e1df7
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-02-06 12:01:57 -0800

    added reverse

---
+++ reverse.js
@@ -0,0 +1,16 @@
+//Given a 32-bit signed integer, reverse digits of an integer.
+
+const reverse = (x) => {
+  const max = 2146483647;
+  let min = - max - 1;
+  let res = 0;
+  let num = x;
+  while(num !== 0){
+    res = (res * 10) + (num % 10);
+    num = num > 0 ? Math.floor(num / 10) : Math.ceil(num / 10);
+    if(res > max || res < min) return 0;
+  }
+  return res;
+};
+
+console.log(reverse(123))
